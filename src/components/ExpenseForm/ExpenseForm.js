2bbf660d6889355c7d3b78806b7e065f888d83dc
ravi <mooninthedarknight1999@gmail.com>
2023-01-15 16:09:42 +0530

    - useState() with onChange
    - event handler onChange()
    - using onChange with useState together

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const clickHandler = (event) => { console.log(event.target.value)}
+  const [enteredTitle,grabTitle] = useState('');
+  const titleHandler = (event) => { grabTitle(event.target.value)};
+
+  const [enteredAmount,grabAmount] = useState('');
+  const amountHandler = (event) => { grabAmount(event.target.value); };
+
+  const [enteredDate,grabDate] = useState('');
+  const dateHandler = (event) => { grabDate(event.target.value); };
   return (
     <div>
       <form className='formdiv'>
         <div className='new-expense__controls form-control'>
-          <input type="text" onChange={clickHandler}/>
+          <input type="text" onChange={titleHandler}/>
         </div>
         <div className="new-expense__controls form-control">
-          <input type="text" />
+          <input type="text" onChange={amountHandler}/>
         </div>
         <div className="new-expense__controls form-control">
-          <input type="date" />
+          <input type="date" onChange={dateHandler}/>
         </div>
+        <p>{enteredTitle}</p>
+        <p>{enteredAmount}</p>
+        <p>{enteredDate}</p>
         <div className="new-expense__actions">
           <button>Add Expense</button>
         </div>
