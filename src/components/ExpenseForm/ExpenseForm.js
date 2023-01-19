ee9e6898de203f78b71374593a8200f17e3431fc
ravi <mooninthedarknight1999@gmail.com>
2023-01-19 08:05:07 +0530

    some practice useState

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -2,14 +2,25 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const [enteredTitle,grabTitle] = useState('');
-  const titleHandler = (event) => { grabTitle(event.target.value)};
+  const [count,setCount] = useState(0);
+  const countHandler = (event) => {
+    event.preventDefault();
+    setCount(count+1);
+  }
+  const [enteredTitle, grabTitle] = useState('');
+  const titleHandler = (event) => {
+    grabTitle(event.target.value)
+  };
 
-  const [enteredAmount,grabAmount] = useState('');
-  const amountHandler = (event) => { grabAmount(event.target.value); };
+  const [enteredAmount, grabAmount] = useState('');
+  const amountHandler = (event) => {
+    grabAmount(event.target.value);
+  };
 
-  const [enteredDate,grabDate] = useState('');
-  const dateHandler = (event) => { grabDate(event.target.value); };
+  const [enteredDate, grabDate] = useState('');
+  const dateHandler = (event) => { 
+    grabDate(event.target.value); 
+  };
 
   const formHandler = (event) => {
     event.preventDefault();
@@ -22,21 +33,26 @@ const ExpenseForm = () => {
     grabTitle('');
     grabAmount('');
     grabDate('');
+    setCount(0);
   }
   return (
     <div>
       <form className='formdiv'>
         <div className='new-expense__controls form-control'>
-          <input type="text" onChange={titleHandler} value={enteredTitle}/>
+          <input type="text" onChange={titleHandler} value={enteredTitle} />
         </div>
         <div className="new-expense__controls form-control">
-          <input type="text" onChange={amountHandler} value={enteredAmount}/>
+          <input type="text" onChange={amountHandler} value={enteredAmount} />
         </div>
         <div className="new-expense__controls form-control">
-          <input type="date" onChange={dateHandler} value={enteredDate}/>
+          <input type="date" onChange={dateHandler} value={enteredDate} />
+        </div>
+        <div>
+          <p>{count}</p>
+          <button onClick={countHandler}>Counter</button>
         </div>
         <div className="new-expense__actions">
-          <button onClick={formHandler }>Add Expense</button>
+          <button onClick={formHandler}>Add Expense</button>
         </div>
       </form>
     </div>
